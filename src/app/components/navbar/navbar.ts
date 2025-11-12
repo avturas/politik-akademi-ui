6e5ea66f6075b63cceaaca86fb8133c0a75345fc
mehmetfurkanaydin <aydin.mehmetfurkan@gmail.com>
2025-11-12 22:39:49 +0300

    prepare for release

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -10,7 +10,7 @@ import { MatButtonModule } from '@angular/material/button';
   template: `
     <mat-toolbar color="primary">
       <span class="font-bold text-lg cursor-pointer" routerLink="/"
-        >Politik Akademi</span
+        >AVTURAS Akademi</span
       >
     </mat-toolbar>
   `,
