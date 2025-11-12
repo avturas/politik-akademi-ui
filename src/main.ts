1549b67158b168ad81e23077d1acddfae7450966
mehmetfurkanaydin <aydin.mehmetfurkan@gmail.com>
2025-11-12 22:48:46 +0300

    added pwa support

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,12 @@ import { provideRouter } from '@angular/router';
 
 import { App } from './app/app';
 import { routes } from './app/app.routes';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(App, {
-  providers: [provideRouter(routes), provideHttpClient(), provideMarkdown()],
+  providers: [provideRouter(routes), provideHttpClient(), provideMarkdown(), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })],
 });
